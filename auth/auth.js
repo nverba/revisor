05fbdd1a955eaeb0e3ad2663179ff0f792692ec4
nverba <distantstate@gmail.com>
2014-11-02 13:40:34 +0000

    testing re-route

--- auth/auth.js
+++ auth/auth.js
@@ -1,8 +1,25 @@
-var ref = new Firebase("https://revisor.firebaseio.com");
-var authData = ref.getAuth();
+var auth_input = document.getElementById('auth');
+var signin     = document.getElementById('signin');
+var ref        = new Firebase("https://revisor.firebaseio.com");
+var authData   = ref.getAuth();
+
+
 if (authData) {
   // user authenticated with Firebase
   console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
 } else {
-  ref.authWithOAuthRedirect("google", function(error) {});
+
+  console.log('none');
+
+  auth_input.style.display = 'block';
+
+  signin.addEventListener("click", function () {
+
+    console.log('click');
+
+    ref.authWithOAuthRedirect("google", function(error) {
+      console.log('auth');
+    });
+  });
+
 }
