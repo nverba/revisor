b9afb25520df113fbf60743e1ae445932f936076
nverba <distantstate@gmail.com>
2014-11-02 11:46:38 +0000

    added google auth

---
+++ auth/auth.js
@@ -0,0 +1,8 @@
+var ref = new Firebase("https://revisor.firebaseio.com");
+var authData = ref.getAuth();
+if (authData) {
+  // user authenticated with Firebase
+  console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+} else {
+  //ref.authWithOAuthRedirect("google", function(error) {});
+}
